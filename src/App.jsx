ee3c4cdc26cab4413c196213d9a7eca7b658af78
mokshijain22 <mokshijain2209@gmail.com>
2026-04-19 01:13:57 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1584,10 +1584,7 @@ export default function App() {
           ))}
         </div>
 
-        {mode==='repo'     && <RepoPanel ghUser={ghUser} oauthAvailable={oauthAvailable} {...sharedProps}/>}
-        {mode==='profile'  && <ProfilePanel {...sharedProps}/>}
-        {mode==='linkedin' && <LinkedInPanel {...sharedProps}/>}
-        {mode==='resume'   && <ResumePanel {...sharedProps}/>}
+        
       </div>
     </>
   );
